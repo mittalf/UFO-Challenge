c649ddaa1f4ca315b6113671a654ae9576c51172
Falguni Mittal <mittalf@gmail.com>
2021-12-23 12:18:28 -0500

    Updated readme file, added png files

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # UFO-Challenge
 UFO Sightings using JavaScript, html
+
+## Overview of the Project:
+
+### Project Background:
+
+The purpose of this project is to visuaize the UFO sigtings data that is available in a JavaScript file.
+- Create an interactive webpage that allows readers to view the UFO sightings data 
+- Filter the data based on datetime
+
+
+
+### Purpose:
+
+Provide a more in-depth analysis of UFO sightings by allowing users to filter for multiple criteria at the same time. In addition to the date, add table filters for 
+- city 
+- state 
+- country
+- shape
+
+## Resources:
+
+Data Source: [data.js](static/js/data.js)  - A JavaScript file with UFO sighting information including date, city, state, country, shape.
+
+Software: JavaScript (ECMA script or ES 2015 or ES6+), D3.js, html, Visual Studio Code
+
+## Results:
+
+Use JavaScript to display data as a table and add filters to manipulate the data.  
+
+- Describe to Dana how someone might use the new webpage by walking her through the process of using the search criteria. 
+
+When the user first visits the web page (index.html), the user is presented with all of the UFO sightings information that is availalble on the data.js JavaScript file.  
+![initialLoad.png](static/images/initialLoad.png)
+
+Since there is too much data displayed on the web page to make sense of it, the user can filter the data to see only the data he is interested in.  There are 5 different filters available on the webpage - datetime, city, state, country, shape. Each filter is already pre-filled with a sample text for users to use as an example. 
+
+There are two different ways:
+
+1) Use one of the available filters (datetime, city, state, country, shape) to filter the data.  
+
+For example if the user filters the data by state (or), then all the data that matches the "or" will be displayed on the web page.
+![filterByState.png](static/images/filterByState.png)
+
+
+2) Use combination of filters to filter the data.
+
+For example the user can enter the value in the date cell ("1/1/2010) as well as the state cell ("CA). The web page will display all the data that matches these two criteria.
+
+![filterByDateState.png](static/images/filterByDateState.png)
+
+## Summary:
+
+One drawback of this webpage:
+
+- Currently to reset the filters, the user needs to refresh the page to reload the inital data. To make this webpage more user friendly, we can add a "clear all" button on the web page to clear all the filters.
+
+Two additional recommendations for further development:
+
+- Currently the user is required to enter (type in) a filter in the form.  This can be prone to errors as the user can misspell the word or might not know all the avialble options.  A better design would be to request the user to choose from availalbe options.
+
+For example, in stead of requesting the user to enter the 2 digit state code, the webpage can show all the avaialble states to choose from using a drop-downbox.
+
+For exaple, instead of asking the user to enter a shape, the webpage should display all the avaialble shapes as a checkbox. A User then simply has to "check" one of the shapes. 
+
+
+
---
+++ static/js/app.js
@@ -0,0 +1,108 @@
+// from data.js
+const tableData = data;
+
+
+// get table references
+var tbody = d3.select("tbody");
+
+function buildTable(data) {
+  // First, clear out any existing data
+  tbody.html("");
+
+  // Next, loop through each object in the data
+  // and append a row and cells for each value in the row
+  data.forEach((dataRow) => {
+    // Append a row to the table body
+    let row = tbody.append("tr");
+
+    // Loop through each field in the dataRow and add
+    // each value as a table cell (td)
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+      cell.text(val);
+    });
+  });
+}
+
+// 1. Create a variable to keep track of all the filters as an object.
+var filters = {};
+
+// 3. Use this function to update the filters. 
+function updateFilters() {
+
+    // 4a. Save the element that was changed as a variable.
+    let changedElement = d3.select(this);
+
+    // 4b. Save the value that was changed as a variable.
+    let elementValue = changedElement.property("value");
+    console.log(elementValue);
+
+    // 4c. Save the id of the filter that was changed as a variable.
+    let filterId = changedElement.attr("id");
+    console.log(filterId);
+  
+    // 5. If a filter value was entered then add that filterId and value
+    // to the filters list. Otherwise, clear that filter from the filters object.
+    if (elementValue){
+      filters[filterId] = elementValue;
+    }
+    else {
+      delete filters[filterId]; 
+    }
+  
+    // 6. Call function to apply all filters and rebuild the table
+    filterTable();
+  
+}
+  
+  // 7. Use this function to filter the table when data is entered.
+  function filterTable() {
+  
+    // 8. Set the filtered data to the tableData.
+    var filteredData = tableData;
+  
+    // 9. Loop through all of the filters and keep any data that
+    // matches the filter values
+    
+    let numFilters = Object.keys(filters).length
+    console.log("in filter tables function - number of filters " + Object.keys(filters).length);
+   
+
+
+    // for (let i=0; i < (numFilters);i++){
+    //   console.log("inside for loop num " + numFilters + "i = " + i);
+        
+      if (filters["datetime"]){
+        filteredData = filteredData.filter(row => row.datetime === filters["datetime"]);
+        console.log("filter by datetime");
+      }
+      if (filters["city"]){ 
+         filteredData = filteredData.filter(row => row.city === filters["city"]);
+         console.log("filter by city");
+       }
+      if (filters["state"]){  
+        filteredData = filteredData.filter(row => row.state == filters["state"]);
+        console.log("filter by state");
+        
+      }
+      if (filters["country"]){  
+        filteredData = filteredData.filter(row => row.country === filters["country"]);
+        console.log("filter by country");
+      }
+      if (filters["shape"]) {   
+       filteredData = filteredData.filter(row => row.shape === filters["shape"]);
+       console.log("filter by shape");
+      }
+            
+     // filteredData = filteredData.filter(row => ((row.datetime === filters["datetime"]) || (row.city === filters["city"]) || (row.state === filters["state"]) || (row.country === filters["country"]) || (row.shape === filters["shape"])));
+    // }
+  
+    // 10. Finally, rebuild the table using the filtered data
+    buildTable(filteredData);
+  }
+  
+  // 2. Attach an event to listen for changes to each filter
+  d3.selectAll("input").on("change", updateFilters);
+  
+  // Build the table when the page loads
+  buildTable(tableData);
